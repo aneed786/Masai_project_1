821d75e78b60e64825064e147682f8397e631996
Mohd Danish <113216768+Danish0611@users.noreply.github.com>
2022-11-08 16:14:08 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
-let reg_email= "abc23@gmail.com";
+let reg_email= "abc@gmail.com";
 let reg_pass= "78657865";
+
+let reg_email = "abc@gmail.com";
+let reg_pass="78657865";
+let  enter_email = "abc@gmail.com"
+let enter_pass = "78657865"
+if (reg_email == enter email && reg_pass == enter pass) {
+console.log("Login Successful");
+} else {
+console.log("Login Fail");
+}
